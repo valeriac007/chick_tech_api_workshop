c29da2b5089660bcf30efde3fde969cd9a092292
valeriac007 <45127342+valeriac007@users.noreply.github.com>
2018-11-17 15:01:12 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -13,10 +13,13 @@ $(document).ready(function(){
     console.log("call-api button was pressed");
 
     // call the API
-    var url = ""; // add your API's URL here!
+    var url = "https://api.nasa.gov/planetary/apod?api_key	=B8UNxTRKr7FDWLcVDCYgJTpbr1Lpg6FnkpqGT2qE&date=2002-1-25&hd=true"; // add your API's URL here!
     $.get(url, function(data, status){
         console.log("Data: " + data + "\nStatus: " + status);
         console.log(data);
+        $("img").attr("src", data.url);
+        $("p").html(data.explanation);
+
         // do something cool with the data here!!!
     });
 
